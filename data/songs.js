f49334028fcb40db55efd43fd313af5851e3b5f8
raintree-commits <rifqiaziz7@gmail.com>
2026-01-04 09:39:30 +0700

    lagu

---
+++ data/songs.js
@@ -0,0 +1,34 @@
+const songs = [
+  {
+    title: "ILU",
+    embed: "https://open.spotify.com/embed/track/4mBzfzsfsDkZ0TzmuJEiZ2"
+  },
+  {
+    title: "Boku no Aurora",
+    embed: "https://open.spotify.com/embed/track/5MQMSgVCzhUruxxAkVzK5H"
+  },
+  {
+    title: "Tsumari",
+    embed: "https://open.spotify.com/embed/track/5z0W1shvZp5NGGI25Oniy2"
+  },
+  {
+    title: "3 Second Rule",
+    embed: "https://open.spotify.com/embed/track/1Cmp9PkwMbcKwJUdJaAee1"
+  },
+  {
+    title: "Ren'ai Henkaku Katsuyou",
+    embed: "https://open.spotify.com/embed/track/6MOfU2rS6rBBrRwt78gAsm"
+  },
+  {
+    title: "Sukidayo to Docchi ga Sakini Iunoka?",
+    embed: "https://open.spotify.com/embed/track/5Xr6WaDKhUfgULKQLoGvBE"
+  },
+  {
+    title: "Genki Yoho",
+    embed: "https://open.spotify.com/embed/track/7jW6T9MCQVgxJJvXyf7K6x"
+  },
+  {
+    title: "Teiten Kansoku",
+    embed: "https://open.spotify.com/embed/track/0Tu4xsLz3NJdm8imn7uJtr"
+  }
+];
