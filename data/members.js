a13591d1266bb652d2d0bacff23486e78fa10e38
raintree-commits <rifqiaziz7@gmail.com>
2026-01-04 09:36:44 +0700

    Add members data with details and images

---
+++ data/members.js
@@ -0,0 +1,106 @@
+<script>
+const members = [
+  {
+    id: 1,
+    name: { id: "Asamiya Hinata", en: "Asamiya Hinata", jp: "朝宮日向" },
+    img: "assets/img/members/Asamiya_Hinata.jpeg",
+    rating: 1000
+  },
+  {
+    id: 2,
+    name: { id: "Ayase Kotori", en: "Ayase Kotori", jp: "綾瀬ことり" },
+    img: "assets/img/members/Ayase_Kotori.jpeg",
+    rating: 1000
+  },
+  {
+    id: 3,
+    name: { id: "Ichihara Tsumugi", en: "Ichihara Tsumugi", jp: "市原紬希" },
+    img: "assets/img/members/Ichihara_Tsumugi.jpeg",
+    rating: 1000
+  },
+  {
+    id: 4,
+    name: { id: "Endo Rino", en: "Endo Rino", jp: "遠藤莉乃" },
+    img: "assets/img/members/Endi_Rino.jpeg", // Sesuai list: Endi_Rino.jpeg
+    rating: 1000
+  },
+  {
+    id: 5,
+    name: { id: "Katase Manaka", en: "Katase Manaka", jp: "片瀬真花" },
+    img: "assets/img/members/Katase_Manaka.jpeg",
+    rating: 1000
+  },
+  {
+    id: 6,
+    name: { id: "Kato Shu", en: "Kato Shu", jp: "加藤柊" },
+    img: "assets/img/members/Kato_Shu.jpeg",
+    rating: 1000
+  },
+  {
+    id: 7,
+    name: { id: "Kurosawa Karen", en: "Kurosawa Karen", jp: "黒澤禾恋" },
+    img: "assets/img/members/Kurosawa_Karen.jpeg",
+    rating: 1000
+  },
+  {
+    id: 8,
+    name: { id: "Sato Rica", en: "Sato Rica", jp: "佐藤莉華" },
+    img: "assets/img/members/Sato_Rica.jpeg",
+    rating: 1000
+  },
+  {
+    id: 9,
+    name: { id: "Suzuno Mio", en: "Suzuno Mio", jp: "鈴野みお" },
+    img: "assets/img/members/Suzuno_Mio.jpeg",
+    rating: 1000
+  },
+  {
+    id: 10,
+    name: { id: "Nakamata Miki", en: "Nakamata Miki", jp: "仲俣美希" },
+    img: "assets/img/members/Nakamata_Miki.jpeg",
+    rating: 1000
+  },
+  {
+    id: 11,
+    name: { id: "Nagase Mari", en: "Nagase Mari", jp: "永瀬真梨" },
+    img: "assets/img/members/Nagase_Mari.jpeg",
+    rating: 1000
+  },
+  {
+    id: 12,
+    name: { id: "Niino Fuka", en: "Niino Fuka", jp: "新野楓果" },
+    img: "assets/img/members/Niino_Fuka.jpeg",
+    rating: 1000
+  },
+  {
+    id: 13,
+    name: { id: "Hashimoto Maki", en: "Hashimoto Maki", jp: "橋本真希" },
+    img: "assets/img/members/Hashimoto_Maki.jpeg",
+    rating: 1000
+  },
+  {
+    id: 14,
+    name: { id: "Hayama Rico", en: "Hayama Rico", jp: "葉山莉瑚" },
+    img: "assets/img/members/Hayama_Rico.jpeg",
+    rating: 1000
+  },
+  {
+    id: 15,
+    name: { id: "Mizuno Noa", en: "Mizuno Noa", jp: "水野乃愛" },
+    img: "assets/img/members/Mizuno_Noa.jpeg",
+    rating: 1000
+  },
+  {
+    id: 16,
+    name: { id: "Momose Suzuna", en: "Momose Suzuna", jp: "百瀬紗菜" },
+    img: "assets/img/members/Momose_Suzuna.jpeg",
+    rating: 1000
+  },
+  {
+    id: 17,
+    name: { id: "Yoshikawa Umi", en: "Yoshikawa Umi", jp: "吉川海未" },
+    img: "assets/img/members/Yoshikawa_Umi.jpeg",
+    rating: 1000
+  }
+];
+</script>
