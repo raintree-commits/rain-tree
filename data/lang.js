934c819d831491518d1eb6a97f94797d0b0629ea
raintree-commits <rifqiaziz7@gmail.com>
2026-01-04 09:41:16 +0700

    baahasa

---
+++ data/lang.js
@@ -0,0 +1,20 @@
+const langData = {
+  id: {
+    homeTitle: "Rain Tree Fan Tools",
+    sorter: "Sorter Member",
+    love: "Ramalan Cinta",
+    song: "Sorter Lagu"
+  },
+  en: {
+    homeTitle: "Rain Tree Fan Tools",
+    sorter: "Member Sorter",
+    love: "Love Fortune",
+    song: "Song Sorter"
+  },
+  jp: {
+    homeTitle: "Rain Tree ファンツール",
+    sorter: "メンバーソート",
+    love: "恋愛占い",
+    song: "楽曲ソート"
+  }
+};
